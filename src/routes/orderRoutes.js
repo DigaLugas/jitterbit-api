01a562f8663e4fe4338c66d192898a0b788c8c75
DigaLugas <lucasgustavorodrigues10@gmail.com>
2026-03-08 14:44:35 -0300

    fix: corrige ordem das rotas para /list não conflitar com /:id

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -4,8 +4,8 @@ const orderController = require('../controllers/orderController');
 const validarPedido = require('../middlewares/validarPedido');
 
 router.post('/', validarPedido, orderController.criar);
+router.get('/list', orderController.listar);
 router.get('/:id', orderController.buscar);
-router.get('/list', orderController.listar); //deve vir antes de /:id
 router.put('/:id', validarPedido, orderController.atualizar);
 router.delete('/:id', orderController.deletar);
 
